3e6a7392fe7b82bf81480c80e9d1d1946ed68df8
XMuli <xmulitech@gmail.com>
2021-10-05 19:39:57 +0800

    build: VS2017 build .bat

--- build.bat
+++ build.bat
@@ -6,7 +6,7 @@ if "%1"=="delete" goto _DEL
 if not exist build md build
 
 cd build
-cmake -DCMAKE_BUILD_TYPE=RelWithDebInfo -G "Visual Studio 15 2017" -A Win32 ../src
+cmake -G "Visual Studio 15 2017" -A Win32 ..
 goto _END
 
 :_DEL
