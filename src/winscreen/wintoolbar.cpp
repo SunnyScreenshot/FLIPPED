df4eb0a40c2a333ccdd81c89cd862aec1821f831
XMuli <xmulitech@gmail.com>
2021-11-17 19:27:32 +0800

    UI: add some toolbar impage(SVG)

--- src/winscreen/wintoolbar.cpp
+++ src/winscreen/wintoolbar.cpp
@@ -22,7 +22,7 @@ WinToolBar::~WinToolBar()
 
 void WinToolBar::init()
 {
-    m_toolBtnName /*<< "rectangle"
+    m_toolBtnName << "rectangle"
                << "ellipse"
                << "line"
                << "arrow"
@@ -30,34 +30,31 @@ void WinToolBar::init()
                << "text"
                << "mosaic"
                << "revoke"
-               << "update" */
                << "download"
                << "copy";
 
 #if defined(Q_OS_MACOS)
-    QStringList listToolTip = {/*tr("矩形（⌘ + 1）")
-                           , tr("椭圆（⌘ + 2）")
-                           , tr("直线（⌘ + 3）")
-                           , tr("箭头（⌘ + 4）")
-                           , tr("笔（⌘ + 5）")
-                           , tr("文字（⌘ + 6）")
-                           , tr("模糊（⌘ + 7）")
-                           , tr("撤销（⌘ + z）")
-                           , tr("上传（⌃ + ⇧ + 1）")
-                           , */ tr("保存（⌘ + s）")
-                           , tr("复制到剪切板 (return)") };
+    QStringList listToolTip = {tr("rectangle (⌘ + 1)")
+                           , tr("ellipse (⌘ + 2)")
+                           , tr("line (⌘ + 3)")
+                           , tr("arrow (⌘ + 4)")
+                           , tr("pen (⌘ + 5）")
+                           , tr("text (⌘ + 6)")
+                           , tr("mosaic (⌘ + 7)")
+                           , tr("revoke (⌘ + z)")
+                           , tr("download (⌘ + S)")
+                           , tr("copy (⌘ + C)")};
 #else
-    QStringList listToolTip = {/*tr("矩形（Ctrl + 1）")
-                           , tr("椭圆（Ctrl + 2）")
-                           , tr("直线（Ctrl + 3）")
-                           , tr("箭头（Ctrl + 4）")
-                           , tr("笔（Ctrl + 5）")
-                           , tr("文字（Ctrl + 6）")
-                           , tr("模糊（Ctrl + 7）")
-                           , tr("撤销（Ctrl + z）")
-                           , tr("上传（Ctrl + Shift + 1）")
-                           , */ tr("保存（Ctrl + S）")
-                           , tr("复制到剪切板 (Ctrl + C)")};
+    QStringList listToolTip = {tr("rectangle (Ctrl + 1)")
+                           , tr("ellipse (Ctrl + 2)")
+                           , tr("line (Ctrl + 3)")
+                           , tr("arrow (Ctrl + 4)")
+                           , tr("pen (Ctrl + 5）")
+                           , tr("text (Ctrl + 6)")
+                           , tr("mosaic (Ctrl + 7)")
+                           , tr("revoke (Ctrl + z)")
+                           , tr("download (Ctrl + S)")
+                           , tr("copy (Ctrl + C)")};
 #endif
 
     m_vecToolBar.fill(nullptr, m_toolBtnName.count());
@@ -77,8 +74,15 @@ void WinToolBar::init()
         m_vecToolBar[i]->setIcon(QIcon(name));
         m_vecToolBar[i]->setIconSize(QSize(16, 16) * WinFullScreen::getScale());
         m_vecToolBar[i]->setToolTip(listToolTip[i]);
-        m_vecToolBar[i]->setCheckable(false);
         m_vecToolBar[i]->setChecked(false);
+
+        // TODO 2021.11.17 后优化为 list 的最后三项
+        if (m_vecToolBar[i]->objectName() == "revoke"
+                || m_vecToolBar[i]->objectName() == "download"
+                || m_vecToolBar[i]->objectName() == "copy")
+            m_vecToolBar[i]->setCheckable(false);
+        else
+            m_vecToolBar[i]->setCheckable(true);
 
         qInfo()<<"===========>"<<name << m_vecToolBar[i]->iconSize()<<rect();
         hLayout->addWidget(m_vecToolBar[i]);
--- src/winscreen/xdraw.h
+++ src/winscreen/xdraw.h
@@ -5,12 +5,13 @@
 #define XDRAW_H
 
 #include <QObject>
+#include <QPen>
 #include <QRect>
 
 // C++11 新增带作用域的枚举，用 enum class  或enum struct（两者等价）声明。
 // https://blog.csdn.net/luckysym/article/details/1666114
 enum class XDrawType {
-    Creator,
+    Draw,
     Move,
     Delete,
     Wateing,
@@ -19,9 +20,11 @@ enum class XDrawType {
 
 struct  XDrawStep
 {
-    uint nId = 0;             // 绘画序号
+    uint nId = 0;                  // 绘画序号
     QRect rtSour = QRect();        // 源位置
     QRect rtDest = QRect();        // 目标位置
+    QPen m_pen = QPen(Qt::NoPen);
+    QBrush m_brush = QBrush(Qt::NoBrush);
     XDrawType drawType = XDrawType::Unknow;  // 绘画类型
 };
 
@@ -33,7 +36,14 @@ public:
     virtual ~XDraw();
 
 private:
+    bool drawStep();
+    bool saveDrawStep(QVector<XDrawStep *>& steps);
+    bool revoke();
+    bool revoke(XDrawStep* step);
+
+private:
     XDrawStep m_step;
+    QVector<XDrawStep *> m_vDrawStep;
 };
 
 #endif // XDRAW_H
