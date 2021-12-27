938dfe582395f5f2d3ac7f3ba89d1917383a4e59
XMuli <xmulitech@gmail.com>
2021-12-27 22:54:19 +0800

    feat: add class subabstoolbar
    subabstoolbar -> submosaictoolbar

---
+++ src/winscreen/subabstoolbar.cpp
@@ -0,0 +1,91 @@
+//
+// Created by xmuli on 2021/12/27.
+//
+#include "subabstoolbar.h"
+#include "screenshot.h"
+#include "../xglobal.h"
+#include "../widget/xverticalline.h"
+#include <QLabel>
+#include <QToolButton>
+
+SubAbsToolBar::SubAbsToolBar(QWidget *parent)
+    : QWidget(parent)
+    , m_tbAbs(new QToolButton(this))
+    , m_tbFilledAbs(new QToolButton(this))
+{
+    const int iconWidth = ICON_HEIGHT * ScreenShot::getScale();
+    m_tbAbs->setIconSize(QSize(iconWidth, iconWidth));
+    m_tbAbs->setToolButtonStyle(Qt::ToolButtonIconOnly);
+    m_tbAbs->setAutoRaise(true);
+    m_tbAbs->setChecked(true);
+
+    m_tbFilledAbs->setIconSize(QSize(iconWidth, iconWidth));
+    m_tbFilledAbs->setToolButtonStyle(Qt::ToolButtonIconOnly);
+    m_tbFilledAbs->setAutoRaise(true);
+    m_tbFilledAbs->setChecked(true);
+
+    setContentsMargins(0, 0, 0, 0);
+    const int margin = 0;
+    QHBoxLayout* hLayout = new QHBoxLayout();
+    hLayout->setObjectName("hLayout");
+    hLayout->setContentsMargins(margin, margin, margin, margin);
+    hLayout->setSpacing(0);
+    hLayout->addWidget(m_tbAbs);
+    hLayout->addWidget(m_tbFilledAbs);
+
+    const int spacing = 4; // 分割线周围的间距
+    hLayout->addSpacing(spacing);
+    hLayout->addWidget(new XVerticalLine(iconWidth, this));
+    hLayout->addSpacing(spacing);
+
+    setLayout(hLayout);
+
+    connect(m_tbAbs, &QToolButton::clicked, this, &SubAbsToolBar::onClicked);
+    connect(m_tbFilledAbs, &QToolButton::clicked, this, &SubAbsToolBar::onClicked);
+}
+
+void SubAbsToolBar::initAbsTb(QString tbPath, QString tbToolTip)
+{
+    if (tbPath.isEmpty() && !m_tbAbs)
+        return;
+
+    m_tbAbs->setIcon(QIcon(tbPath));
+    m_tbAbs->setToolTip(tbToolTip);
+}
+
+void SubAbsToolBar::initFilledAbsTb(QString tbPath, QString tbToolTip)
+{
+    if (tbPath.isEmpty() && !m_tbFilledAbs)
+        return;
+
+    m_tbFilledAbs->setIcon(QIcon(tbPath));
+    m_tbFilledAbs->setToolTip(tbToolTip);
+}
+
+void SubAbsToolBar::onClicked(bool checked)
+{
+    Q_UNUSED(checked);
+
+    if (sender() == m_tbAbs)
+        emit sigIsFill(false);
+    else if (sender() == m_tbFilledAbs)
+        emit sigIsFill(true);
+}
+
+void SubAbsToolBar::addWidget(QWidget *widget)
+{
+    QHBoxLayout* hLayout = findChild<QHBoxLayout *>("hLayout");
+    if (!hLayout)
+        return;
+
+    hLayout->addWidget(widget);
+}
+
+void SubAbsToolBar::addStretch(int stretch)
+{
+    QHBoxLayout* hLayout = findChild<QHBoxLayout *>("hLayout");
+    if (!hLayout)
+        return;
+
+    hLayout->addStretch(stretch);
+}
---
+++ src/winscreen/subabstoolbar.h
@@ -0,0 +1,38 @@
+//
+// Created by xmuli on 2021/12/27.
+//
+#ifndef SUBABSTOOLBAR_H
+#define SUBABSTOOLBAR_H
+
+#include <QWidget>
+
+QT_BEGIN_NAMESPACE
+class QToolButton;
+QT_END_NAMESPACE
+
+class SubAbsToolBar : public QWidget
+{
+    Q_OBJECT
+public:
+    explicit SubAbsToolBar(QWidget *parent = nullptr);
+    void addWidget(QWidget *widget);
+    void addStretch(int stretch);
+
+    virtual void initUI() = 0;
+
+protected:
+    void initAbsTb(QString tbPath, QString tbToolTip);
+    void initFilledAbsTb(QString tbPath, QString tbToolTip);
+
+signals:
+    void sigIsFill(bool bFill);
+
+public slots:
+    void onClicked(bool checked);
+
+private:
+    QToolButton* m_tbAbs;
+    QToolButton* m_tbFilledAbs;
+};
+
+#endif // SUBABSTOOLBAR_H
