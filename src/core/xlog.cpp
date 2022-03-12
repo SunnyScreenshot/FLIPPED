621c6cdf97d085768f9974425042f58290ad6504
XMuli <xmulitech@gmail.com>
2022-03-13 00:39:40 +0800

    feat: try using spdlog

---
+++ src/core/xlog.cpp
@@ -0,0 +1,12 @@
+﻿/*******************************************************************
+ * Copyright (C)  2022  偕臧  All rights reserved.
+ *
+ * Blog:   https://ifmet.cn
+ * GitHub: https://github.com/XMuli
+ * Author: 偕臧 <xmulitech@gmail.com>
+ *
+ * Create: 2022.03.12
+ * Modify: 2022.01.12
+ * Description: 使用 spdlog 来生成日志
+ ******************************************************************/
+#include "xlog.h"
--- src/main.cpp
+++ src/main.cpp
@@ -16,38 +16,12 @@
 //#include "./wininfo/wininfo_win.h"
 #include <iostream>
 
-#include "spdlog/spdlog.h"
+#include "spdlog/logger.h"
+#include "spdlog/sinks/basic_file_sink.h"
 
 int main(int argc, char *argv[])
 {
-	
-    spdlog::info("Welcome to spdlog!");
-    spdlog::error("Some error message with arg: {}", 1);
 
-    spdlog::warn("Easy padding in numbers like {:08d}", 12);
-    spdlog::critical("Support for int: {0:d};  hex: {0:x};  oct: {0:o}; bin: {0:b}", 42);
-    spdlog::info("Support for floats {:03.2f}", 1544.23456);
-    spdlog::info("Positional args are {1} {0}..", "too", "supported");
-    spdlog::info("{:<30}", "left aligned");
-
-    spdlog::set_level(spdlog::level::debug); // Set global log level to debug
-    spdlog::debug("This message should be displayed..");
-
-    // change log pattern
-    spdlog::set_pattern("[%H:%M:%S %z] [%n] [%^---%L---%$] [thread %t] %v");
-
-    spdlog::info("Welcome to spdlog!");
-    spdlog::error("Some error message with arg: {}", 1);
-
-    spdlog::warn("Easy padding in numbers like {:08d}", 12);
-    spdlog::critical("Support for int: {0:d};  hex: {0:x};  oct: {0:o}; bin: {0:b}", 42);
-    spdlog::info("Support for floats {:03.2f}", 1544.23456);
-    spdlog::info("Positional args are {1} {0}..", "too", "supported");
-    spdlog::info("{:<30}", "left aligned");
-    // Compile time log levels
-    // define SPDLOG_ACTIVE_LEVEL to desired level
-    SPDLOG_TRACE("Some trace message with param {}", 42);
-    SPDLOG_DEBUG("Some debug message");
 
     // getAllTopWinRect();
 
@@ -63,13 +37,34 @@ int main(int argc, char *argv[])
     /* 枚举窗口的所有进程 */
     //获取屏幕上所有的顶层窗口,每发现一个窗口就调用回调函数一次
 
-
- //   qInfo()<<QObject::tr("Program running program start.");
  
-//	setlocale(LC_CTYPE, "");
-//	WinInfoWin info;
-//	std::vector<WinInfo> vec;
-//	info.getAllWinInfo(vec);
+    auto logger = spdlog::basic_logger_mt("basic_logger", "logs/basic-log.txt");
+    logger->flush_on(spdlog::level::err);
+    //spdlog::fl flush_every(std::chrono::seconds(5));
+
+    logger->info("Welcome to spdlog!");
+    logger->error("Some error message with arg: {}", 1);
+    logger->warn("Easy padding in numbers like {:08d}", 12);
+    logger->critical("Support for int: {0:d};  hex: {0:x};  oct: {0:o}; bin: {0:b}", 42);
+    logger->info("Support for floats {:03.2f}", 1.23456);
+    logger->info("Positional args are {1} {0}..", "too", "supported");
+    logger->info("{:<30}", "left aligned");
+    logger->set_level(spdlog::level::debug); // Set global log level to debug
+    logger->debug("This message should be displayed..");
+
+    // change log pattern
+    logger->set_pattern("[%H:%M:%S %z] [%n] [%^---%L---%$] [thread %t] %v");
+
+
+    logger->info("Welcome to spdlog!");
+    logger->error("Some error message with arg: {}", 1);
+    logger->warn("Easy padding in numbers like {:08d}", 12);
+    logger->critical("Support for int: {0:d};  hex: {0:x};  oct: {0:o}; bin: {0:b}", 42);
+    logger->info("Support for floats {:03.2f}", 1.23456);
+    logger->info("Positional args are {1} {0}..", "too", "supported");
+    logger->info("{:<30}", "left aligned");
+    logger->set_level(spdlog::level::debug); // Set global log level to debug
+    logger->debug("This message should be displayed..");
 
     // 截图、显示主界面；若点击右上角，则整程序关闭; 如同执行了 close、destroy 一类函数
     Tray* tary = new Tray();
