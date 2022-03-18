b84dee04ac7c567b748c8e33ccca29c9563d75b5
XMuli <xmulitech@gmail.com>
2022-03-19 00:32:19 +0800

    feat: Wrapping spdlog and using macros to make it easier to use

---
+++ src/core/xlog/xlog.h
@@ -0,0 +1,88 @@
+﻿/*******************************************************************
+ * Copyright (C)  2022  偕臧  All rights reserved.
+ *
+ * GitHub: https://github.com/XMuli
+ * Author: 偕臧 <xmulitech@gmail.com>
+ *
+ * Create: 2022.03.12
+ * Modify: 2022.01.12
+ * Description: 使用 spdlog 来生成日志
+ ******************************************************************/
+#ifndef XLOG_H
+#define XLOG_H
+
+#ifdef Q_OS_WIN
+    #define SPDLOG_WCHAR_TO_UTF8_SUPPORT  // 需要定义在 spdlog 之前，以支持 wchar_t 的输出
+#endif
+
+#include <spdlog/spdlog.h>
+#include "spdlog/sinks/rotating_file_sink.h"
+#include <QString>
+
+class XLog
+{
+public:
+    static XLog* instance() {
+        static XLog instance;
+        return &instance;
+    }
+
+    std::shared_ptr<spdlog::logger> getLogger() {
+        return m_logger;
+    }
+
+private:
+    XLog() {
+        m_logger = spdlog::rotating_logger_mt("fileLogger", "picshot.log", 1024 * 1024 * 20, 10, true);
+        //m_logger->set_pattern("%Y-%m-%d %H:%M:%S.%f <thread %t> [%l] [%@] %v");
+
+        std::string level = "debug";
+        if (level == "trace") {
+            m_logger->set_level(spdlog::level::trace);
+            m_logger->flush_on(spdlog::level::trace);
+        } else if (level == "debug") {
+            m_logger->set_level(spdlog::level::debug);
+            m_logger->flush_on(spdlog::level::debug);
+        } else if (level == "info") {
+            m_logger->set_level(spdlog::level::info);
+            m_logger->flush_on(spdlog::level::info);
+        } else if (level == "warn") {
+            m_logger->set_level(spdlog::level::warn);
+            m_logger->flush_on(spdlog::level::warn);
+        } else if (level == "error") {
+            m_logger->set_level(spdlog::level::err);
+            m_logger->flush_on(spdlog::level::err);
+        } else if (level == "critical") {
+            m_logger->set_level(spdlog::level::critical);
+            m_logger->flush_on(spdlog::level::critical);
+        } else if (level == "off") {
+            m_logger->set_level(spdlog::level::off);
+            m_logger->flush_on(spdlog::level::off);
+        } else {
+            m_logger->set_level(spdlog::level::n_levels);
+        }
+    }
+
+    ~XLog() {
+        spdlog::drop_all(); // must do this
+    }
+
+    XLog(XLog&&) = delete;
+    XLog(const XLog&) = delete;
+    void operator= (const XLog&) = delete;
+
+private:
+    std::shared_ptr<spdlog::logger> m_logger;
+};
+
+// use embedded macro to support file and line number
+#define XLOG_TRACE(...)    SPDLOG_LOGGER_CALL(XLog::instance()->getLogger().get(), spdlog::level::trace, __VA_ARGS__)
+#define XLOG_DEBUG(...)    SPDLOG_LOGGER_CALL(XLog::instance()->getLogger().get(), spdlog::level::debug, __VA_ARGS__)
+#define XLOG_INFO(...)     SPDLOG_LOGGER_CALL(XLog::instance()->getLogger().get(), spdlog::level::info, __VA_ARGS__)
+#define XLOG_WARN(...)     SPDLOG_LOGGER_CALL(XLog::instance()->getLogger().get(), spdlog::level::warn, __VA_ARGS__)
+#define XLOG_ERROR(...)    SPDLOG_LOGGER_CALL(XLog::instance()->getLogger().get(), spdlog::level::err, __VA_ARGS__)
+#define XLOG_CRITICAL(...) SPDLOG_LOGGER_CALL(XLog::instance()->getLogger().get(), spdlog::level::critical, __VA_ARGS__)
+#define XLOG_OFF(...)      SPDLOG_LOGGER_CALL(XLog::instance()->getLogger().get(), spdlog::level::off, __VA_ARGS__)
+
+
+#endif // XLOG_H
--- src/main.cpp
+++ src/main.cpp
@@ -16,9 +16,7 @@
 //#include "./wininfo/wininfo_win.h"
 #include <iostream>
 
-#include "spdlog/logger.h"
-#include "spdlog/sinks/basic_file_sink.h"
-
+#include "core/xlog/xlog.h"
 int main(int argc, char *argv[])
 {
 
@@ -37,34 +35,18 @@ int main(int argc, char *argv[])
     /* 枚举窗口的所有进程 */
     //获取屏幕上所有的顶层窗口,每发现一个窗口就调用回调函数一次
 
- 
-    auto logger = spdlog::basic_logger_mt("basic_logger", "logs/basic-log.txt");
-    logger->flush_on(spdlog::level::err);
-    //spdlog::fl flush_every(std::chrono::seconds(5));
 
-    logger->info("Welcome to spdlog!");
-    logger->error("Some error message with arg: {}", 1);
-    logger->warn("Easy padding in numbers like {:08d}", 12);
-    logger->critical("Support for int: {0:d};  hex: {0:x};  oct: {0:o}; bin: {0:b}", 42);
-    logger->info("Support for floats {:03.2f}", 1.23456);
-    logger->info("Positional args are {1} {0}..", "too", "supported");
-    logger->info("{:<30}", "left aligned");
-    logger->set_level(spdlog::level::debug); // Set global log level to debug
-    logger->debug("This message should be displayed..");
+    XLOG_DEBUG("1Welcome to spdlog!");
+    XLOG_DEBUG("1Support 测试中文for int: {0:d};  hex: {0:x};  oct: {0:o}; bin: {0:b}", 42);
+    XLOG_INFO("3Welcome to spdlog!");
+    XLOG_INFO("4Support 测试中文for int: {0:d};  hex: {0:x};  oct: {0:o}; bin: {0:b}", 42);
 
-    // change log pattern
-    logger->set_pattern("[%H:%M:%S %z] [%n] [%^---%L---%$] [thread %t] %v");
-
-
-    logger->info("Welcome to spdlog!");
-    logger->error("Some error message with arg: {}", 1);
-    logger->warn("Easy padding in numbers like {:08d}", 12);
-    logger->critical("Support for int: {0:d};  hex: {0:x};  oct: {0:o}; bin: {0:b}", 42);
-    logger->info("Support for floats {:03.2f}", 1.23456);
-    logger->info("Positional args are {1} {0}..", "too", "supported");
-    logger->info("{:<30}", "left aligned");
-    logger->set_level(spdlog::level::debug); // Set global log level to debug
-    logger->debug("This message should be displayed..");
+#ifdef Q_OS_WIN
+    XLOG_DEBUG(L"2Welcome to spdlog!");
+    XLOG_INFO(L"3Welcome to spdlog!");
+    XLOG_DEBUG(L"2Support 测试中文for int: {0:d};  hex: {0:x};  oct: {0:o}; bin: {0:b}", 42);
+    XLOG_INFO(L"4Support 测试中文for int: {0:d};  hex: {0:x};  oct: {0:o}; bin: {0:b}", 42);
+#endif
 
     // 截图、显示主界面；若点击右上角，则整程序关闭; 如同执行了 close、destroy 一类函数
     Tray* tary = new Tray();
