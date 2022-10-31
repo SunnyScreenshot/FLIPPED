baf3cf972b7dadb09666b383c53276ba1f4272f4
XMuli <xmulitech@gmail.com>
2022-10-31 19:09:19 +0800

    chroe: update .gitignore

--- src/main.cpp
+++ src/main.cpp
@@ -67,7 +67,7 @@ int main(int argc, char *argv[])
     QString qmPath(qApp->applicationDirPath());
 
 #if defined(Q_OS_MAC)
-    qmPath += "/../../../../src/translations/" + lanuage + ".qm";
+    qmPath += "/../../../../bin/translations/" + lanuage + ".qm";
 #elif defined(Q_OS_WIN)
     if (QString(_COMPILER_ID).compare("MSVC") == 0)
         qmPath += "/../translations/" + lanuage + ".qm";
